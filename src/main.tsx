26547a46aa2398425b68aa505597e4fed326256b
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-01-16 15:59:57 -0500

    Change Chakra UI version

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "@/components/ui/provider";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
 );
